f2611a28fa6595b8761d71d6a5075151e9010db8
Denis Lazarev <mahrizh@gmail.com>
2026-02-08 13:39:03 +0200

    More docs for extensions

--- Runtime/ReactiveUtility.cs
+++ Runtime/ReactiveUtility.cs
@@ -6,24 +6,28 @@ namespace ReactiveObjects
 	{
 		#region ListenNow
 
+		/// <summary> Subscribes on change and immediately invokes listener. </summary>
 		public static void ListenNow(this IReactive reactive, Action listener)
 		{
 			reactive.Listen(listener);
 			listener.InvokeSafe();
 		}
 
+		/// <summary> Subscribes on value change and immediately invokes listener with current value. </summary>
 		public static void ListenNow<TValue>(this IReadOnlyReactive<TValue> reactive, Action<TValue> listener)
 		{
 			reactive.Listen(listener);
 			listener.InvokeSafe(reactive.Value);
 		}
 
+		/// <summary> Subscribes on any value change of any key and immediately invokes listener. </summary>
 		public static void ListenNow<TKey, TValue>(this IReadOnlyReactive<TKey, TValue> reactive, Action listener)
 		{
 			reactive.Listen(listener);
 			listener.InvokeSafe();
 		}
 
+		/// <summary> Subscribes on any value change of any key and immediately invokes listener with current key-value pairs. </summary>
 		public static void ListenNow<TKey, TValue>(this IReadOnlyReactive<TKey, TValue> reactive, Action<TKey, TValue> listener)
 		{
 			reactive.Listen(listener);
@@ -31,12 +35,14 @@ namespace ReactiveObjects
 				listener.InvokeSafe(key, value);
 		}
 
+		/// <summary> Subscribes on value change of specific key and immediately invokes listener. </summary>
 		public static void ListenNow<TKey, TValue>(this IReadOnlyReactive<TKey, TValue> reactive, TKey key, Action listener)
 		{
 			reactive.Listen(key, listener);
 			listener.InvokeSafe();
 		}
 
+		/// <summary> Subscribes on value change of specific key and immediately invokes listener with current value of that key. </summary>
 		public static void ListenNow<TKey, TValue>(this IReadOnlyReactive<TKey, TValue> reactive, TKey key, Action<TValue> listener)
 		{
 			reactive.Listen(key, listener);
@@ -47,6 +53,7 @@ namespace ReactiveObjects
 
 		#region ListenOnce
 
+		/// <summary> Subscribes on only single next change. </summary>
 		public static Listening ListenOnce(this IReactive reactive, Action listener)
 		{
 			return Listening.Create(reactive, Wrapper);
@@ -57,6 +64,7 @@ namespace ReactiveObjects
 			}
 		}
 
+		/// <summary> Subscribes on only single next value change. </summary>
 		public static Listening ListenOnce<TValue>(this IReadOnlyReactive<TValue> reactive, Action<TValue> listener)
 		{
 			return Listening.Create(reactive, Wrapper);
@@ -67,6 +75,7 @@ namespace ReactiveObjects
 			}
 		}
 
+		/// <summary> Subscribes on only single next change of any key. </summary>
 		public static Listening ListenOnce<TKey, TValue>(IReactive<TKey, TValue> reactive, Action listener)
 		{
 			return Listening.Create(reactive, Wrapper);
@@ -77,6 +86,7 @@ namespace ReactiveObjects
 			}
 		}
 
+		/// <summary> Subscribes on only single next value change of any key. </summary>
 		public static Listening ListenOnce<TKey, TValue>(IReactive<TKey, TValue> reactive, Action<TKey, TValue> listener)
 		{
 			return Listening.Create(reactive, Wrapper);
@@ -87,6 +97,7 @@ namespace ReactiveObjects
 			}
 		}
 
+		/// <summary> Subscribes on only single next change of specific key. </summary>
 		public static Listening ListenOnce<TKey, TValue>(this IReadOnlyReactive<TKey, TValue> reactive, TKey key, Action listener)
 		{
 			return Listening.Create(reactive, key, Wrapper);
@@ -97,6 +108,7 @@ namespace ReactiveObjects
 			}
 		}
 
+		/// <summary> Subscribes on only single next value change of specific key. </summary>
 		public static Listening ListenOnce<TKey, TValue>(this IReadOnlyReactive<TKey, TValue> reactive, TKey key, Action<TValue> listener)
 		{
 			return Listening.Create(reactive, key, Wrapper);
